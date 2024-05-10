c0638f9514dffd5dafcff06650bdbb1c13ba0725
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-05-10 08:18:33 -0500

    cambios menores de angular

--- app-productos/src/app/app.config.ts
+++ app-productos/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+//Aquí se importan las rutas
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
