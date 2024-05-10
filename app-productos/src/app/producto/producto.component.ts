d6749355fbb1bb5998c99ed9fa1e862fe46f467f
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-05-10 09:03:55 -0500

    Componente

---
+++ app-productos/src/app/producto/producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-producto',
+    template: `
+        <h1>Mi componente (hijo) Producto</h1>
+        <p>Este es un componente de prueba</p>
+    `
+})
+export class ProductoComponent {
+    // Puedes añadir lógica del componente aquí si es necesario
+}
